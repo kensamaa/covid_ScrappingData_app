4ca27448beea731ee5a914cd815faf36a88e2e08
kensamaa <amineab203@gmail.com>
2020-06-01 22:06:27 +0100

    adding the map

--- lib/mapPage.dart
+++ lib/mapPage.dart
@@ -1,58 +1,137 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
+import 'package:geolocator/geolocator.dart';
+
 class mapPage extends StatefulWidget {
   @override
   _mapPageState createState() => _mapPageState();
 }
 
 class _mapPageState extends State<mapPage> {
+
+  MapController controller=new MapController();
+  Position position;
+  final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+  Position _currentPosition;
+  List<Marker> markers;
+  //String _currentAddress;
+
+  void initState(){
+    super.initState();
+    _getCurrentLocation();
+  }
+  _getCurrentLocation() {
+    geolocator
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
+        .then((Position position) {
+      setState(() {
+        _currentPosition = position;
+      });
+      print("location");
+      print(_currentPosition.latitude.runtimeType);
+      print(_currentPosition.longitude.runtimeType);
+
+
+    }).catchError((e) {
+      print(e);
+    });
+  }
+  getPosition()async{
+     position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: <Widget>[
           Container(
-            height: 200,
+            height: 100,
             width: double.infinity,
             decoration:
             BoxDecoration(
               gradient: LinearGradient(
                   colors: [
-                    Colors.amberAccent,
-                    Colors.amber
+                    Colors.deepPurple,
+                    Colors.deepPurpleAccent
                   ]
               ),
               color: Colors.black,
-              borderRadius: BorderRadius.circular(25),
-              border: Border.all(
-                color: Color(0xFFE5E5E5),
-              ),
+              //borderRadius: BorderRadius.circular(25),
+              //border: Border.all(
+              //  color: Color(0xFFE5E5E5),
+              //),
             ),
+            //child: Text((_currentPosition.latitude+_currentPosition.longitude).toString()),
           ),
 
-          SizedBox(
-            height: 20,
-          ),
+
             Container(
-              height: 550,
+              height: 670,
 
               child: FlutterMap(
+                mapController: controller,
                 options: new MapOptions(
-                  center: new LatLng(31.7858387, -9.3939546),
-                  //zoom: 23.0,
-                  minZoom: 10.0
+                  center: new LatLng(_currentPosition.latitude,_currentPosition.longitude),
+                  zoom: 13,
+                  //minZoom: 10.0
                 ),
                 layers: [
                   new TileLayerOptions(
                       urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
                       subdomains: ['a', 'b', 'c']
                   ),
+                  new MarkerLayerOptions(
+                    markers: [
+                      Marker(//this is user marker
+                          height: 30,
+                          width: 30,
+                          point: new LatLng(_currentPosition.latitude, _currentPosition.longitude),
+                          builder: (ctx) =>Icon(Icons.pin_drop,color: Colors.red)
+
+                      ),
+                      Marker(//this is emsi
+                          height: 30,
+                          width: 30,
+                          point: new LatLng(33.541505, -7.673544),
+                          builder: (ctx) =>Icon(Icons.pin_drop,color: Colors.green)
+                      ),
+                      Marker(//this is hospital chaikh zaid
+                          height: 30,
+                          width: 30,
+
+                          point: new LatLng(33.5523758, -7.6699494),
+                          builder: (ctx) =>Container(
+                            child: Text("chaikh khalifa"),
+                          ),
+                      ),
+
+                    ],
+                  ),
+                  new CircleLayerOptions(
+                    circles: [
+                      CircleMarker(
+                        point: new LatLng(_currentPosition.latitude, _currentPosition.longitude),
+                        radius: 10,
+                        color: Colors.green,
+                        borderColor: Colors.red,
+                        borderStrokeWidth: 2,
+                        useRadiusInMeter: false,
+
+                      )
+                    ]
+                  )
+
                 ],
               ),
             ),
         ],
       ),
     );
+
   }
+
+
 }
